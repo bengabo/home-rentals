7c299bcc1d15cbe48cf595d9dcb7a61f681a278a
Bengabo <ben@bengabo.com>
2020-06-11 16:11:46 +0200

    homes component and routes created

---
+++ src/app/homes/homes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-homes',
+  templateUrl: './homes.component.html',
+  styleUrls: []
+})
+export class HomesComponent implements OnInit {
+
+  homes$ = this.dataService.getHomes();
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+  }
+
+}
