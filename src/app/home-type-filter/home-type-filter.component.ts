07eb0a98fb10acd0e3625ea2bf2e609b11ff6903
Bengabo <ben@bengabo.com>
2020-06-15 15:36:32 +0200

    router in home-type-filter component

--- src/app/home-type-filter/home-type-filter.component.ts
+++ src/app/home-type-filter/home-type-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home-type-filter',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTypeFilterComponent implements OnInit {
 
-  constructor() { }
+  @Output() applied = new EventEmitter();
+  
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      Apartment: [],
+      Room: [],
+      House: []
+    })
   }
 
+  submit(formValue) {
+    
+    const homeTypes = Object.keys(formValue).filter(item =>
+      formValue[item]); 
+
+      this.applied.emit(homeTypes);
+    }
 }
--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homes',
@@ -12,9 +13,16 @@ export class HomesComponent implements OnInit {
 
   homes$ = this.dataService.getHomes();
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  homeTypeFilterApplied($event) {
+    this.homeTypeDrowndownOpen = false;
+    this.router.navigate(['homes'], { queryParams: { 'home-type': $event}});
+  }
 }
